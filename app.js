d45d2efef9a9e064cdaf67314298c8e5d2b748c0
james alonso <jaime.alonso@ieee.org>
2020-01-22 20:27:01 -0500

    Initial commit. Hello World nodejs

---
+++ app.js
@@ -0,0 +1,13 @@
+
+//how to declare a function. To run nodejs use in terminal: "node app.js"
+function sayHello(name) {
+    console.log('Hello ' + name);
+}
+
+sayHello('Javi');
+
+
+
+//Window is not defined. We do not have windows in node,
+//this only goes for the browser.
+console.log(window);
